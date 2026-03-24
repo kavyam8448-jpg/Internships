1a631881ed70c787df37eb7ce7e25e430c6286b1
Kavya M <kavyam8448@gmail.com>
2026-03-24 15:08:22 +0530

    Add files via upload

---
+++ assignment 11/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <h1>Multi-Page React App</h1>
+
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ assignment 11/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This app demonstrates routing using React Router.</p>
+    </div>
+  );
+}
+
+export default About;
